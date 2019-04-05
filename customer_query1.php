d58512c37feca54cab778d0ee653b7c709e609d5
Andrew Feng <awfeng1994@hotmail.com>
2019-04-05 19:18:51 -0400

    tested the db connection and it works

---
+++ customer_query1.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "zpc353.encs.concordia.ca";
+$username = "zpc353_4";
+$password = "TKASA353";
+$dbname = "zpc353_4";
+
+// Create Connection
+$conn=new mysqli($servername, $username, $password, $dbname);
+// Check Connection
+if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM Customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>Name</th><th>Phone</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["name"]."</td><td>".$row["phone_number"]."</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
