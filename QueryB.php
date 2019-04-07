69a45f7230e8cc5b31025c1b154447995a137d5b
Subhannita Sarcar <35349991+subhannita1994@users.noreply.github.com>
2019-04-07 05:00:48 -0400

    Update QueryB.php

--- QueryB.php
+++ QueryB.php
@@ -9,7 +9,7 @@ $conn=new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
 }
-$sql = "select title, author, edition, publisher, date_placed from Orders, Orderitem, Book where Orders.order_number=Orderitem.order_number and Orderitem.isbn=Book.isbn and ((date_received is not null and DATEDIFF( Orders.date_received, Orders.date_placed) > 21) or (date_received is null and DATEDIFF(  NOW(), Orders.date_placed) > 21))";
+$sql = "select title from Book where isbn in (select isbn from Orderitem where order_number in (select Orders.order_number from Orders left join satisfied_by on Orders.order_number=satisfied_by.order_number where satisfied_by.order_number is null))";
 $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     echo "<table><tr><th>Book Title</th></tr>";
